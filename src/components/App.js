075eac25223ba1ced65b7ec5c3c426d20269f327
deliverydriver <kashmirhost@me.com>
2018-11-15 14:27:44 -0700

    Initial commit, I'm composing my first weather app, hardcoded then API, any comments appreciated.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../App.css';
+import front from './front';
+
+class App extends Component {
+  render() {
+    return (
+      <front />
+    );
+  };
+};
+
+export default App;
